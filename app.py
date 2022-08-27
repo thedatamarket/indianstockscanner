134989e35b55551b127afe29c007dc042ba8c53e
tanny <tanmoymunshi1999@gmail.com>
2022-08-27 13:55:08 +0530

    MMI Integration

--- app.py
+++ app.py
@@ -455,7 +455,22 @@ def analysisapi(name,period,interval):
     df = df.rename({'index': 'Date'}, axis=1)
     return Response(df.to_json(), mimetype='application/json')
     # return df.to_html()
-    
+
+#MMI
+@app.route('/mmi') 
+def mmi():
+    URL = 'https://www.tickertape.in/market-mood-index'
+    HEADERS = ({'User-Agent':
+                'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 \
+                (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',\
+                'Accept-Language': 'en-US, en;q=0.5'})
+  
+    webpage = requests.get(URL, headers=HEADERS)
+    soup = BeautifulSoup(webpage.content, "html.parser")
+    dom = etree.HTML(str(soup))
+    mmi = dom.xpath('//*[@id="app-container"]/div/div[1]/div[1]/div/div[2]/span')[0].text
+    day = dom.xpath('//*[@id="app-container"]/div/div[1]/div[1]/div/div[2]/p/text()[2]')[0]
+    return render_template("MMI.html",mmi_page = mmi,last_updated = day)   
 
 
 
